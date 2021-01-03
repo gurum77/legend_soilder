52c2863fead0a0cb18aac3ac6ab4a006cc1e9a2d
gurum77 <hileejaeho@gmail.com>
2021-01-03 13:40:57 +0900

    map

---
+++ scenes/MapManager.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+var maps = [
+	"res://maps/HorizontalVillidgeMap.tscn", 
+	"res://maps/VerticalVillidgeMap.tscn"]
+
+
+
+func _ready():
+	# 기존 maps 은 제거
+	var exist_maps = get_children()
+	for map in exist_maps:
+		map.queue_free()
+	# 나눠서 남는거의 -1을 한다.
+	# -1이 되면 마지막 인덱스로 사용
+	var idx = StaticData.current_stage / maps.size() as int
+	idx = StaticData.current_stage - (maps.size() * idx)
+	if idx < 0:
+		idx = maps.size() - 1
+	var ins = load(maps[idx]).instance()
+	add_child(ins)
+		
