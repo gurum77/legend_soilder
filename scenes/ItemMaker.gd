02a663a69474a9f0a6356718550f28b160a4f323
Evan <leejaeho@empal.com>
2021-01-11 06:48:54 +0900

    money spread

--- scenes/ItemMaker.gd
+++ scenes/ItemMaker.gd
@@ -1,20 +1,14 @@
 extends Node2D
 
-var factor = 0.1
+var factor = 0.01
 
-var max_dist = 30
+
 func make_item(hp):
 	var amount = hp * factor
 	var count = amount / Define.default_money_amount
 	if count < 0:
 		count = 1
 	for i in count:
-		var dist = rand_range(0, 1) * max_dist
-		var dir = Vector2(rand_range(0, 1), rand_range(0, 1))
-		
 		var ins = Preloader.money.instance()
 		ins.global_position = global_position
-		get_tree().root.call_deferred("add_child", ins)
-		$Tween.interpolate_property(ins, "global_position", ins.global_position, ins.global_position + dir * dist, 0.5, Tween.TRANS_EXPO, Tween.EASE_IN)
-	$Tween.start()
-	
+		get_tree().root.call_deferred("add_child", ins);
--- scripts/Money.gd
+++ scripts/Money.gd
@@ -2,7 +2,15 @@ extends Area2D
 
 
 var money_amount = Define.default_money_amount
+var max_dist = 30
 
+func _ready():
+	randomize()
+	var dist = rand_range(0, 1) * max_dist
+	var dir = Vector2(rand_range(0, 1), rand_range(0, 1))
+	$Tween.interpolate_property(self, "global_position", global_position, global_position + dir * dist, 0.5, Tween.TRANS_EXPO, Tween.EASE_IN)
+	$Tween.start()
+	
 func _on_Money_body_entered(body):
 	if body is PlayerBody:
 		StaticData.total_money += money_amount
