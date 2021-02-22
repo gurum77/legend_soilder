d6b5cb10e0a4e0bb07ba60c0615d2a4aefc5ccb8
gurum77 <hileejaeho@gmail.com>
2021-02-22 23:31:23 +0900

    balance

--- scripts/Table.gd
+++ scripts/Table.gd
@@ -2,13 +2,13 @@ extends Node
 
 # stage 클리어 점수 펙터
 var stage1_clear_store = 6000
-var stage_clear_score_factor = 1.12
+var stage_clear_score_factor = 1.15
 
 # 적 체력 증가 펙터
 var enemy_hp_factor = 1.05
 
 # player hp factor
-var player_hp_factor = 1.07
+var player_hp_factor = 0.1
 
 # weapon power factor
 var weapon_power_factor = 1.2
@@ -21,14 +21,14 @@ var upgrade1_cost = 1000	# 첫번째 upgrade 비용
 var upgrade_cost_factor = 1.3	# 이후 upgrade시 늘어나는 비용 factor
 
 # 무기별 interval
-var interval_Pistol = 0.5
+var interval_Pistol = 0.4
 var interval_SMG = 0.25
 var interval_MG = 0.2
 var interval_FlameThrower = 0.15
 var interval_RPG = 1
 
 # player의 기본 공격력
-var player_power = 300
+var player_power = 500
 # player의 기본 체력
 var player_hp = 3000#300000
 
@@ -36,8 +36,15 @@ var player_hp = 3000#300000
 var power_Pistol = 700
 var power_SMG = 500
 var power_MG = 700
-var power_FlameThrower = 400
-var power_RPG = 2000
+var power_FlameThrower = 300
+var power_RPG = 1200
+
+# level별 upgrade factor
+var factor_by_level = 0.1
+
+# 적 hp / power의 최대 업그레이드 level
+var max_enemy_level = 200
+
 
 # 무기별 interval
 func get_weapon_interval_by_level(weapon):
@@ -61,11 +68,14 @@ func get_weapon_interval_by_level(weapon):
 # hp level이 반영된 player의 hp 리턴
 func get_player_hp_by_level()->int:
 	var hp_level =  StaticData.get_player_information().hp_level
-	var hp = player_hp
-	for _i in range(hp_level):
-		hp = hp * player_hp_factor
-		
-	return hp as int
+	return player_hp + (player_hp * player_hp_factor) * hp_level
+	 
+#	var hp_level =  StaticData.get_player_information().hp_level
+#	var hp = player_hp
+#	for _i in range(hp_level):
+#		hp = hp * player_hp_factor
+#
+#	return hp as int
 	
 # power level이 반영된 palyer의 공격력 리턴
 func get_player_power_by_level():
@@ -165,7 +175,7 @@ func get_weapon_bullet_distance(weapon) -> int:
 	elif weapon == Define.Weapon.FlameThrower:
 		return 200
 	elif weapon == Define.Weapon.RPG:
-		return 400
+		return 350
 	return 100
 		
 # stage clear에 필요한 점수 리턴
@@ -188,15 +198,20 @@ func get_upgrade_cost(to_level)->int:
 # stage의 난이도 위치 
 # 이걸로 적HP와 clear score가 결정된다.
 func get_stage_position(si:StageInformation)->int:
-	#return (si.level * 1.5) + si.current_step
-	return (si.level * 3) + si.current_step
+	var pos = (si.level * 1.5) + si.current_step
+	if pos > max_enemy_level:
+		pos = max_enemy_level
+	return pos
+#	return (si.level * 3) + si.current_step
 	
 # stage별 적 hp를 리턴
 func get_enemy_hp(hp, si:StageInformation)->int:
 	var pos = get_stage_position(si)
-	for _i in range(pos):
-		hp = hp * enemy_hp_factor
-	return hp as int
+	return hp + (hp * factor_by_level) * pos
+#	var pos = get_stage_position(si)
+#	for _i in range(pos):
+#		hp = hp * enemy_hp_factor
+#	return hp as int
 	
 # player의 level별 기본 공격력 리턴
 func get_player_power(power, power_level)->int:
@@ -204,9 +219,10 @@ func get_player_power(power, power_level)->int:
 	
 # power level별 무기 공격력 리턴
 func get_weapon_power(power, power_level)->int:
-	for _i in range(power_level-1):
-		power = power * weapon_power_factor
-	return power as int
+	return power + (power * factor_by_level) * power_level
+#	for _i in range(power_level-1):
+#		power = power * weapon_power_factor
+#	return power as int
 	
 # interval level별 무기 inerval 리턴
 func get_weapon_interval(interval, interval_level)->int:
